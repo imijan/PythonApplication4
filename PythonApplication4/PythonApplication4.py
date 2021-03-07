f8639c177595fb66695a57049edced6077fba971
imijan <61419872+imijan@users.noreply.github.com>
2021-03-07 18:52:06 +0300

    Добавьте файлы проекта.

---
+++ PythonApplication4/PythonApplication4.py
@@ -0,0 +1,25 @@
+d = int(input('Введите длину стен: '))
+e = int(input('Введите ширину стен: '))
+p1 = (d + e) * 2
+print(p1)
+print('Введите высоту стены')
+p2 = int(p1 * int(input('')))
+print(p2)
+
+print('Площадь деврных и оконных проемов')
+o1 = (int(input()) * int(input()))
+d1 = (int(input()) * int(input()))
+pp = o1 + d1
+print(pp)
+
+print(p2 - pp)
+
+a = int(input('Введите длину: '))
+b = int(input('Введите ширину: '))
+c = int(input('Введите высоту: '))
+
+pb1 = (a * c)
+print(pb1)
+print('Введите полученую площать коробки')
+pk = int(input())
+print((pk / pb1) * 100)
